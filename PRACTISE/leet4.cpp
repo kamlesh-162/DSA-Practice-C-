1f4a84f4995b53555d2ead2d0216c1844c9771d0
kamlesh-162 <kamleshshinde162@gmail.com>
2026-07-05 23:41:48 +0530

    leet code 196

--- PRACTISE/leet4.cpp
+++ PRACTISE/leet4.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main(){
-   int arr[2]={1,2};
+   int arr[4]={2,2,4,4};
+   int num[5]={2,2,2,4,4};
+   float s1=0,s2=0;
+   for (int i = 0; i < 4; i++)
+   {
+    s1=s1+arr[i];
+   }
+   for (int i = 0; i < 5; i++)
+   {
+    s2=s2+num[i];
+   }
+  
+   float ans=(s1+s2)/9;
+   cout<<ans;
   return 0;
 }
