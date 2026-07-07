403f5dc77fd2802b1f5599cc903bdb9b542f7dbd
kamlesh-162 <kamleshshinde162@gmail.com>
2026-07-07 23:27:59 +0530

    link list =insertion at positon and deletion at position

--- 9.Linked list/singly-deletion.cpp
+++ 9.Linked list/singly-deletion.cpp
@@ -12,6 +12,17 @@ class Node{
     this-> next=NULL;
   }
 
+  //destructer
+  ~Node(){
+    int value=this->data;
+    //memory free
+    if(this->next!=NULL){
+      delete next;
+      this->next=NULL;
+    }
+    cout<<"memmory is free for node with data :"<<value<<endl;
+  }
+
 };
 
 void InsertAtHead(Node* &head,int d){
@@ -20,17 +31,14 @@ void InsertAtHead(Node* &head,int d){
   Node* temp=new Node(d);
   temp->next=head;
   head=temp; 
-
-
 }
+
 void InsertAtTail(Node* &tail,int d){
 
   //create new node
   Node* temp=new Node(d);
   tail->next=temp; 
   tail=temp;
-
-
 }
 
 void InsertAtPosition(Node* &tail,Node* &head,int d,int position){
@@ -55,15 +63,46 @@ void InsertAtPosition(Node* &tail,Node* &head,int d,int position){
     return;
   }
   
-
   //createing node for d
   Node* temp=new Node(d);
   temp->next=privious->next;
-  privious->next=temp;
-
-  
+  privious->next=temp;  
 }
 
+void DeletionNode(Node* &head,Node * &tail,int position){
+ 
+  //deletion frist or start node
+  if(position==1){
+    Node* temp=head;
+    head=head->next;
+    // free the memory of head node
+    temp->next=NULL;
+    delete temp;
+  }
+  else{
+   // deleting any middle or last node
+   Node* current=head;
+   Node* privious=NULL;
+   int count=1;
+   while (count<position){
+       privious=current;
+       current=current->next; //travers till (n-1)th
+       count++;
+   }
+   if (current->next==NULL) {
+       privious->next=current->next;
+       current->next=NULL;
+       tail=privious;
+       delete current;
+       return;
+   }
+   
+   privious->next=current->next;
+   current->next=NULL;
+   delete current;
+
+  }
+}
 void print(Node* &head){
    Node* temp=head;
 
@@ -73,6 +112,7 @@ void print(Node* &head){
    }
   cout<<endl;
 }
+
 
 int main(){
 
@@ -98,6 +138,17 @@ int main(){
 
    cout<<"Enter at position (tail)"<<endl;
    InsertAtPosition(tail,head,7,7);
+   print(head);
+
+   cout<<"deletion at start "<<endl;
+   DeletionNode(head,tail,1);
+   print(head);
+
+   cout<<"deletion at position   (4)"<<endl;
+   DeletionNode(head,tail,4);
+   print(head);
+   cout<<"deletion at  tail  (5)"<<endl;
+   DeletionNode(head,tail,5);
    print(head);
 
    cout<<"head :"<<head->data<<endl;
